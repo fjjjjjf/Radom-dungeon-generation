f99e03a9163304ab5521ed6ee65b9c9ff89cbf97
fjjjjjf <853723049@qq.com>
2023-11-02 23:58:52 +0800

    add random room generation

--- dungeon generation/Assets/Scripts/RoomFistDungeonGenerator.cs
+++ dungeon generation/Assets/Scripts/RoomFistDungeonGenerator.cs
@@ -11,7 +11,7 @@ public class RoomFistDungeonGenerator : DungeonRandomGenerator
     [SerializeField]
     private int minRoomWidth = 4, minRoomHeight = 4;
     [SerializeField]
-    private int dungeonWidth=20,dungeonHeight=20;
+    private int dungeonWidth = 20, dungeonHeight = 20;
     [SerializeField]
     [Range(0, 10)]
     private int offset = 1;//避免因为地板而连起来
@@ -25,14 +25,25 @@ public class RoomFistDungeonGenerator : DungeonRandomGenerator
 
     private void createRooms()
     {
-      var roomsList =ProceduralGenerationAlogorithms.BinarySpacePartitioning(new BoundsInt((Vector3Int)startPosition,new Vector3Int
-          (dungeonWidth,dungeonHeight,0)),minRoomWidth,minRoomHeight);
+        var roomsList = ProceduralGenerationAlogorithms.BinarySpacePartitioning(new BoundsInt((Vector3Int)startPosition, new Vector3Int
+            (dungeonWidth, dungeonHeight, 0)), minRoomWidth, minRoomHeight);
 
-        HashSet<Vector2Int> floor =new HashSet<Vector2Int>();
-        floor = createSimpleRooms(roomsList);
+        HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
+
+        if (randomWalkRooms)
+        {
+            floor = CreateRoomsRandomly(roomsList);
+        }
+        else
+        {
+            floor = createSimpleRooms(roomsList);
+        }
+
+
+
 
         //获得房间中心坐标，将房间连接起来
-        List<Vector2Int> roomCenters =new List<Vector2Int>();
+        List<Vector2Int> roomCenters = new List<Vector2Int>();
         foreach (var item in roomsList)
         {
             roomCenters.Add((Vector2Int)(Vector3Int.RoundToInt(item.center)));
@@ -42,13 +53,33 @@ public class RoomFistDungeonGenerator : DungeonRandomGenerator
         floor.UnionWith(corriders);
 
         tilemapVisualizer.PaintFloorTiles(floor);
-        WallGenerator.CreateWalls(floor,tilemapVisualizer);
+        WallGenerator.CreateWalls(floor, tilemapVisualizer);
+    }
+
+    private HashSet<Vector2Int> CreateRoomsRandomly(List<BoundsInt> roomsList)
+    {
+        HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
+        for (int i = 0; i < roomsList.Count; i++)
+        {
+            var roomBounds = roomsList[i];
+            var roomCenter = new Vector2Int(Mathf.RoundToInt(roomBounds.center.x), Mathf.RoundToInt(roomBounds.center.y));
+            var roomFloor = RunRandomWalk(randomWalkSO, roomCenter);
+            foreach (var position in roomFloor)
+            {
+                if (position.x >= (roomBounds.xMin + offset) && position.x <= (roomBounds.xMax - offset)
+                    && position.y >= (roomBounds.yMin + offset) && position.y <= (roomBounds.yMax - offset))
+                {
+                    floor.Add(position);
+                }
+            }
+        }
+        return floor;
     }
 
     private HashSet<Vector2Int> ConnectRooms(List<Vector2Int> roomCenters)
     {
-       HashSet<Vector2Int> corridors =new HashSet<Vector2Int>();
-        var currentRoomCenter = roomCenters[UnityEngine.Random.Range(0,roomCenters.Count)];
+        HashSet<Vector2Int> corridors = new HashSet<Vector2Int>();
+        var currentRoomCenter = roomCenters[UnityEngine.Random.Range(0, roomCenters.Count)];
 
         roomCenters.Remove(currentRoomCenter);
         while (roomCenters.Count > 0)
@@ -69,18 +100,19 @@ public class RoomFistDungeonGenerator : DungeonRandomGenerator
         HashSet<Vector2Int> corridor = new HashSet<Vector2Int>();
         var position = currentRoomCenter;
         corridor.Add(position);
-        while(position.y != destination.y) {
-            if(position.y < destination.y)
+        while (position.y != destination.y)
+        {
+            if (position.y < destination.y)
             {
                 position += Vector2Int.up;
             }
-            else if( position.y > destination.y)
+            else if (position.y > destination.y)
             {
                 position += Vector2Int.down;
             }
             corridor.Add(position);
         }
-        while(position.x !=destination.x)
+        while (position.x != destination.x)
         {
             if (position.x < destination.x)
             {
@@ -98,7 +130,7 @@ public class RoomFistDungeonGenerator : DungeonRandomGenerator
     private Vector2Int FindClosestPointTo(Vector2Int currentRoomCenter, List<Vector2Int> roomCenters)
     {
         Vector2Int closest = Vector2Int.zero;
-        float distance =float.MaxValue;
+        float distance = float.MaxValue;
         //遍历找到最短
         foreach (var position in roomCenters)
         {
@@ -117,9 +149,9 @@ public class RoomFistDungeonGenerator : DungeonRandomGenerator
         HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
         foreach (var room in roomsList)
         {
-            for(int col=offset;col<room.size.x-offset;col++)
+            for (int col = offset; col < room.size.x - offset; col++)
             {
-                for(int row =offset;row<room.size.y-offset;row++)
+                for (int row = offset; row < room.size.y - offset; row++)
                 {
                     Vector2Int position = (Vector2Int)room.min + new Vector2Int(col, row);
                     floor.Add(position);
